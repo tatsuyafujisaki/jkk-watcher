19c9fe60d18849f73146788af25e30e798869f40
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2019-03-25 23:57:05 +0900

    Update index.js to fix a glitch

--- index.js
+++ index.js
@@ -1,53 +1,47 @@
 const puppeteer = require('puppeteer');
+const sleep = require('sleep')
 
 async function hasVacantRoom(url) {
   const browser = await puppeteer.launch();
+
   let page = await browser.newPage();
 
   await page.goto(url);
   await page.close();
 
   page = await new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page()))); // Get a page created by window.open
-  await page.waitFor('table', {timeout: 3000});
-  const html = await page.evaluate(() => document.body.innerText);
+  await page.waitFor('table', { timeout: 3000 });
 
-  const hasVacantRoom = html.includes('該当しました');
+  sleep.sleep(1);
+
+  const content = await page.evaluate(() => document.body.innerText.trim());
+
+  const hasVacantRoom = content.includes('内見'); // Be sure this script is saved as UTF-8.
 
   await browser.close();
 
   return hasVacantRoom;
 }
 
-async function openJKK(url) {
-  const browser = await puppeteer.launch({headless: false}); // default is true
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.close();
+async function doIt(url) {
+  if (await hasVacantRoom(url)) {
+    const browser = await puppeteer.launch({ headless: false }); // default is true
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.close();
+  }
 }
 
-(async () => {
-  const jutakuNames = [
-    // Nakamurakita
-    '30B330FC30B730E430CF30A430E030CA30AB30E030E930AD30BF',
+// Nakamurakita
+doIt('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyokenDirect?jutaku_name=30B330FC30B730E430CF30A430E030CA30AB30E030E930AD30BF');
 
-    // Kotakecho
-    '30B330FC30B730E330CF30A430E030B330BF30B130C130E730A6',
+// Kotakecho
+doIt('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyokenDirect?jutaku_name=30B330FC30B730E330CF30A430E030B330BF30B130C130E730A6')
 
-    // Chihaya
-    '30B330FC30B730E330CF30A430E030C130CF30E4',
+// Chihaya
+doIt('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyokenDirect?jutaku_name=30B330FC30B730E330CF30A430E030C130CF30E4');
 
-    // Kasumidai
-    // '30AB30B930DF30C030A4',
-  ];
-
-  const baseUrl = 'https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyokenDirect?jutaku_name=';
-
-  for (i = 0; i < jutakuNames.length; i++) {
-    const url = baseUrl + jutakuNames[i];
-    if (await hasVacantRoom(url)) {
-      openJKK(url);
-    }
-  }
-})();
+// Kasumidai
+doIt('https://jhomes.to-kousya.or.jp/search/jkknet/service/akiyaJyokenDirect?jutaku_name=30AB30B930DF30C030A4');
 
 // await page.screenshot({path: 'screenshot.png', fullPage: true});
